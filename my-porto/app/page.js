ad6141193482d6ba51da51ac65aaeebdb4de32bf
aakmal.f <akmalfauzi001@gmail.com>
2024-01-21 16:51:20 +0900

    set up first

---
+++ my-porto/app/page.js
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        hey, I'm Akmal 👋
+      </h1>
+    </div>
+  );
+}
+
+export default page;
---
+++ my-porto/app/projects/page.js
@@ -0,0 +1,10 @@
+
+const projectsPage = () => {
+    
+    return(
+    <div>
+        <h1>Projects Page</h1>
+    </div>
+)}
+
+export default projectsPage;
