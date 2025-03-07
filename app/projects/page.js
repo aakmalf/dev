04b742f315f4bafa296f0b108cee24a7a17aac30
aakmalf <akmalfauzi001@gmail.com>
2025-03-08 01:49:23 +0700

    update repo structure

---
+++ app/projects/page.js
@@ -0,0 +1,16 @@
+import Card from "../components/card";
+
+const projectsPage = () => {
+  return (
+    <div>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        read my projects
+      </h1>
+      <>
+        <Card />
+      </>
+    </div>
+  );
+};
+
+export default projectsPage;
---
+++ app/work/page.js
@@ -0,0 +1,9 @@
+const workPage = () => {
+  return (
+    <div>
+      <h1>Work Page</h1>
+    </div>
+  );
+};
+
+export default workPage;
